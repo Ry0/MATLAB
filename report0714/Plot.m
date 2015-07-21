0035c4ae3290f145225a04b8a8f99f9ea0475792
Ry0 <k104037r@mail.kyutech.jp>
2015-07-21 23:09:27 +0900

    課題着手

--- report0714/Plot.m
+++ report0714/Plot.m
@@ -7,6 +7,8 @@ sim('Laplace') % simulinkの読み込み
 figure(1);
 xlim([0,40]);
 ylim([-0.5,3.5])
+xlabel('t');
+ylabel('x');
 
 orange = [241/255,98/255,2/255];
 green = [0/255,179/255,104/255];
@@ -14,8 +16,6 @@ green = [0/255,179/255,104/255];
 y_analytical = 3/2-(31/34)*exp(-4*t)+(24/17)*cos(t)+(6/17)*sin(t);
 hold on;
 
-xlabel('t');
-ylabel('x');
 plot(t,y,'Color',green); % simulinkによって出力された値
 plot(t,y_analytical,'Color',orange); % 解析的解
-hold on;
+
